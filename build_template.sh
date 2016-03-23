a143f0259d238d1cdd5be8b146d537eb9b64b588
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2016-03-23 01:34:39 +0000

    Release v2.0 The template is completely done in this version. The folder /template will release the final template based on the subjects provided. It is still needed reorganize the bvec files in order to follow the registration process.

--- build_template.sh
+++ build_template.sh
@@ -35,7 +35,7 @@ for subj in `ls $1 | grep "subj*"`; do
 
   # Apply dcm2nii to create the nii files from the DICOM FOLDERS
   # Starting to select each subject to do all the process.
-  source preprocessing/dcm2nii.sh $1 $subj
+  source config/dcm2nii.sh $1 $subj
 
   # nii folder has all the images to process
   NII_FOLDER=$1/$subj/nii
@@ -46,12 +46,12 @@ for subj in `ls $1 | grep "subj*"`; do
     # Show the parameters choosen: registration (flirt -> DOF (affine, 6 dof, 3 dof), fnirt (warpsize, fhwmin, fwhmref, subsamp, ))
     # Read conf file with the variables values.
     echo "  --> Creating brain mask..."
-    BET_THR=`cat config/config_var.conf | grep BET_THR | cut -c9-12`
-    NECK_RM=`cat config/config_var.conf | grep NECK_RM | cut -c9-10`
+    BET_THR=`cat config/config_var.conf | grep BET_THR= | cut -c9-12`
+    NECK_RM=`cat config/config_var.conf | grep NECK_RM= | cut -c9-10`
     source preprocessing/brain_extraction.sh $NII_FOLDER $acq $BET_THR $NECK_RM
 
     echo "  --> Eddy current correction procedure..."
-    HIGH_PERF=`cat config/config_var.conf | grep HIGH_PERF | cut -c11-12`
+    HIGH_PERF=`cat config/config_var.conf | grep HIGH_PERF= | cut -c11-12`
     source preprocessing/eddy_correction.sh  $NII_FOLDER $acq $HIGH_PERF
   done
 
@@ -60,36 +60,28 @@ for subj in `ls $1 | grep "subj*"`; do
     DOF=`cat config/config_var.conf | grep DOF | cut -c5-6`
     source preprocessing/reg_intrasubj.sh $NII_FOLDER $DOF
 
-    echo ""
     echo "  --> Spatial normalization - Registration with MNI template"
-    MNI=`cat config/config_var.conf | grep DOF | cut -c5-16`
-    source preprocessing/reg_standard $NII_FOLDER $MNI
+    MNI=`cat config/config_var.conf | grep MNI= | cut -c5-20`
+    BET_THR=`cat config/config_var.conf | grep BET_THR= | cut -c9-12`
+    WARP=`cat config/config_var.conf | grep WARP= | cut -c6-12`
+    INFWHM=`cat config/config_var.conf | grep INFWHM= | cut -c8-11`
+    REFFWHM=`cat config/config_var.conf | grep REFFWHM= | cut -c9-12`
+    SUBSAMP=`cat config/config_var.conf | grep SUBSAMP= | cut -c9-17`
+    source preprocessing/reg_standard $NII_FOLDER $MNI $BET_THR $WARP $INFWHM $REFFWHM $SUBSAMP
 
-# TODO Finish the registration to MNI template!!!!
-
-  # Extracting first 3D volume to be referenced
-    # echo "Step 3: Registration procedure: Initial linear approximation"
-  #   echo "  --> Extracting b0 volume..."
-  #   # fslroi resolve this
-  #
-
-#   echo "  --> Non-diffusion volume (B0)"
-#   # Initialize registration process from b0 volume
-#   #   ->Use FLIRT initially.
-#   echo "  --> Apply affine transformation on DWIs volumes"
-#   #   ->Apply on all DWIs volumes applyxfm
-#
-# echo "Step 4: Registration procedure: Initial non linear approximation"
-# echo "  --> Non-diffusion volume (B0)"
-# # Again with the b0 volume
-# #   ->Use FNIRT  (fnirt --ref=MNI --in=b0_ec_brain_MNI --iout=b0_ec_brain_MNI_2mm_spline --fout=b0_spline_field --warpres=8,8,8 --infwhm=0 --reffwhm=0 --subsamp=2,1,1,1
-# echo "  --> Apply non linear warping on DWIs volumes"
-# #   ->Use applywarp -i dti_ec_brain_MNI.nii.gz -r MNI152 -o dti_ec_brain_MNI_spline.nii.gz -w b0_ec_brain_MNI_2mm_warpcoef.nii.gz
-#
-echo "***********************************************************************"
-echo "*** DTI normalization from $subj terminated with success.***"
-echo "***********************************************************************"
-echo ""
-echo ""
-
+    echo "***********************************************************************"
+    echo "*** DTI normalization from $subj terminated with success.***"
+    echo "***********************************************************************"
+    echo ""
+    echo ""
 done
+
+    echo "Step 4: Merge all subjects to build a final brain template"
+    if [[ `ls $1 | grep template` == "" ]]; then
+      mkdir $1/template
+    fi
+    for subj in `ls $1 | grep subj`; do
+      cp $1/$subj/nii/dti_ec_corr_mean_nlin.nii.gz $1/template
+      mv $1/template/dti_ec_corr_mean_nlin.nii.gz $1/template/dti_$subj.nii.gz
+    done
+    source postprocessing/merge_temp $1/template
